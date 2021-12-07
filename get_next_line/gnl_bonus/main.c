9dd64d2b655c85edc7e7f4075ed75c5c1dccb347
Soyoung JUNG <sojung@e2r13p14.clusters.42paris.fr>
2021-12-07 12:13:27 +0100

    bonus main fixed

--- get_next_line/gnl_bonus/main.c
+++ get_next_line/gnl_bonus/main.c
@@ -6,7 +6,7 @@
 /*   By: sojung <sojung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 17:45:52 by sojung            #+#    #+#             */
-/*   Updated: 2021/12/06 18:16:20 by sojung           ###   ########.fr       */
+/*   Updated: 2021/12/07 12:10:26 by sojung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,23 @@
 
 int	main(void)
 {
-	int	fd[4];
+	int	fd[5];
 	int	i;
-	char	*res[4];
-	fd[0] = open("text.txt", O_RDONLY);
-	fd[1] = open("text2.txt", O_RDONLY);
-	fd[2] = open("t3.txt", O_RDONLY);
-	fd[3] = open("empty_text.txt", O_RDONLY);
-	while ((res[0] = get_next_line(fd[0])) || (res[1] = get_next_line(fd[1])) || (res[2] = get_next_line(fd[2])))
+	char	*res[5];
+	fd[0] = open("a.txt", O_RDONLY);
+	fd[1] = open("b.txt", O_RDONLY);
+	fd[2] = open("c.txt", O_RDONLY);
+	fd[3] = open("d.txt", O_RDONLY);
+	fd[4] = open("e.txt", O_RDONLY);
+	res[0] = get_next_line(fd[0]);
+	res[1] = get_next_line(fd[1]);
+	res[2] = get_next_line(fd[2]);
+	res[3] = get_next_line(fd[3]);
+	res[4] = get_next_line(fd[4]);
+	while (res[0] || res[1] || res[2] || res[3] || res[4])
 	{ 
 		i = 0;
-		while (i < 3)
+		while (i < 5)
 		{
 			if (res[i] != NULL)
 			{
@@ -33,9 +39,16 @@ int	main(void)
 			}
 			i++;
 		}
+		res[0] = get_next_line(fd[0]);
+		res[1] = get_next_line(fd[1]);
+		res[2] = get_next_line(fd[2]);
+		res[3] = get_next_line(fd[3]);
+		res[4] = get_next_line(fd[4]);
 	}
 	close(fd[0]);
 	close(fd[1]);
 	close(fd[2]);
 	close(fd[3]);
+	close(fd[4]);
+	return (0);
 }
